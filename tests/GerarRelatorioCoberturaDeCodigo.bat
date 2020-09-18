6ab4bf6bdbca986044d323af8a31cfc743e0afce
Diego Galante <diegogalantte@gmail.com>
2020-09-17 22:24:54 -0300

    Tradução no arquivo GerarRelatorioCoberturaDeCodigo.bat

--- tests/GerarRelatorioCoberturaDeCodigo.bat
+++ tests/GerarRelatorioCoberturaDeCodigo.bat
@@ -1,34 +1,43 @@
-REM Create a 'GeneratedReports' folder if it does not exist
-if not exist "%~dp0GeneratedReports" mkdir "%~dp0GeneratedReports"
+REM Create a 'RelatoriosGerados' folder if it does not exist
+REM Cria a pasta 'RelatoriosGerados' se não existir
+if not exist "%~dp0RelatoriosGerados" mkdir "%~dp0RelatoriosGerados"
  
 REM Remove any previously created test output directories
+REM Remove qualquer teste anteriormente criado no diretório de saída
 CD %~dp0
 FOR /D /R %%X IN (%USERNAME%*) DO RD /S /Q "%%X"
  
-REM Run the tests against the targeted output
-call :RunOpenCoverUnitTestMetrics
+REM Atribuí o nome do caminho padrão do usuario
+set meuCaminho=C:\Users\%USERNAME%
  
-REM Generate the report output based on the test results
+REM (INATIVADO) Roda os testes novamente do OpenCover para gerar as metricas
+REM call :RunOpenCoverUnitTestMetrics
+call :RodaMetricaDeTestesOpenCover
+ 
+REM Gera a saída do relatório baseado nos resultados dos testes 
 if %errorlevel% equ 0 (
- call :RunReportGeneratorOutput
+ call :GeraRelatorioDeSaida
 )
  
-REM Launch the report
+REM Exibe o relatório
 if %errorlevel% equ 0 (
- call :RunLaunchReport
+ call :ExibeRelatorio
 )
 exit /b %errorlevel%
  
-:RunOpenCoverUnitTestMetrics
-REM OpenCover.Console.exe -register:user -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:test -filter:"+[TDSA.Business*]* -[TDSA.Test*]*" -output:".\MyProject_coverage.xml" -oldstyle
+:RodaMetricaDeTestesOpenCover
+echo "(RodaMetricaDeTestesOpenCover -> Metodo Inativado no momento)" ^
+REM OpenCover.Console.exe -register:user -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:test -filter:"+[TDSA.Business*]* -[TDSA.Test*]*" -output:".\tests\RelatoriosGerados\TDSA_Cobertura.xml" -oldstyle
 exit /b %errorlevel%
  
-:RunReportGeneratorOutput
-"C:\Users\bibos\.nuget\packages\reportgenerator\4.5.6\tools\netcoreapp3.0\ReportGenerator.exe" ^
--reports:"%~dp0GeneratedReports\TDSA_Cobertura.xml" ^
--targetdir:"%~dp0GeneratedReports\ReportGenerator Output"
+:GeraRelatorioDeSaida
+echo "Gera Relatorio de Saída ~\RelatoriosGerados\TDSA_Cobertura.xml"
+"%meuCaminho%\.nuget\packages\reportgenerator\4.6.7\tools\netcoreapp3.0\ReportGenerator.exe" ^
+-reports:"%~dp0RelatoriosGerados\TDSA_Cobertura.xml" ^
+-targetdir:"%~dp0RelatoriosGerados\RelatorioGerado Saida"
 exit /b %errorlevel%
  
-:RunLaunchReport
-start "report" "%~dp0GeneratedReports\ReportGenerator Output\index.htm"
+:ExibeRelatorio
+echo "Exibe Relatório no Browser"
+start "report" "%~dp0RelatoriosGerados\RelatorioGerado Saida\index.htm"
 exit /b %errorlevel%
